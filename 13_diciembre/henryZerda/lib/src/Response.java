d67ce37f5b8476712dfe4234a629e10afcb1be07
henryzrr <henry.zrz@gmail.com>
2019-12-15 19:34:32 -0400

    tarea 16 de diciembre

---
+++ 13_diciembre/henryZerda/lib/src/Response.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class Response {
+    private int status;
+    private List<App> data;
+
+
+    public Response(int status, List<App> data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setData(List<App> data) {
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public List<App>  getData() {
+        return data;
+    }
+
+
+}
---
+++ 13_diciembre/henryZerda/phone/src/Main.java
@@ -0,0 +1,7 @@
+import phone.PhoneKernel;
+
+public class Main {
+    public static void main(String []args){
+        PhoneKernel phoneKernel = new PhoneKernel();
+    }
+}
